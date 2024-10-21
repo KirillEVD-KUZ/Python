41761f381c31dae70bae6046aa9ec4189ad446c4
Андрей Боев <a.boev.it@gmail.com>
2024-10-21 12:20:20 +0300

    6. Списки + 9. Строки

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-def print_seq(begin, end):
-    for i in range(begin, end + 1):
-        print(f'{i} ', end='')
-    print()
+# Сумма всех чисел до n
 
-print_seq(45, 200)
-print_seq(4, 20)
+def sum_sequence(n):
+    if n > 0:
+        return n + sum_sequence(n - 1)
+    else:
+        return 0
+
+
+# print(sum_sequence(10))
+
+def print_sequence(n):
+    if n > 0:
+        print(n, end=' ')
+        print_sequence(n - 1)
+
+
+print_sequence(10)
